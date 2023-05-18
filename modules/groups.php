34dbed0c8aa881fd5316f43dbee6c4620e5ac6fd
Allen Jenkins <allenrjenkins@gmail.com>
2023-05-18 16:59:18 -0700

    Update groups.php

--- modules/groups.php
+++ modules/groups.php
@@ -1,4 +1,4 @@
 <ul class="list-group">
-    <li class="list-group-item"><img src="http://lorempixel.com/20/20/" />&nbsp;&nbsp; <a href="#">Random group 1</a></li>
-    <li class="list-group-item"><img src="http://lorempixel.com/20/20/" />&nbsp;&nbsp; <a href="#">Random group 2</a></li> 
+    <li class="list-group-item"><img src="https://picsum.photos/70/70/" />&nbsp;&nbsp; <a href="#">Random group 1</a></li>
+    <li class="list-group-item"><img src="https://picsum.photos/70/70/" />&nbsp;&nbsp; <a href="#">Random group 2</a></li> 
 </ul>
